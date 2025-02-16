9c6b2556a182b4e465c672d017d2449dfe65b540
DINETHKUMAR <dinethkumardk@gmail.com>
2025-02-17 00:39:01 +0530

    Used object for show scores

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+const rpsscore = {
+  Wins : 0,
+  Lose : 0,
+  Tie : 0
+}
+const htscore = {
+  Wins : 0,
+  Lose : 0,
+  
+}
 function playgame(playermove){
   const computerMove = pickcomputermove();
-  result ='';
+  let result ='';
   if (playermove==='Stone'){
     if (computerMove==='Paper'){
-      result = 'lose';
+      result = 'Lose';
     }else if (computerMove==='Stone'){
       result = 'Tie';
     }if (computerMove==='Scissor'){
       result = 'Win';
     }
-    alert(`Your move is ${playermove} and computer move is ${computerMove}, so you ${result}.`);
+    
   }else if(playermove==='Paper'){
     if (computerMove==='Paper'){
       result = 'Tie';
@@ -18,17 +28,25 @@ function playgame(playermove){
     }if (computerMove==='Scissor'){
       result = 'Lose';
     }
-    alert(`Your move is ${playermove} and computer move is ${computerMove}, so you ${result}.`);
   }else if(playermove==='Scissor'){
     if (computerMove==='Paper'){
       result = 'Win';
     }else if (computerMove==='Stone'){
-      result = 'Tie';
-    }if (computerMove==='Scissor'){
       result = 'Lose';
+    }else if (computerMove==='Scissor'){
+      result = 'Tie';
     }
-    alert(`Your move is ${playermove} and computer move is ${computerMove}, so you ${result}.`);
+    
+  }if (result=='Win'){
+    rpsscore.Wins+=1;
+  }else if (result=='Lose'){
+    rpsscore.Lose+=1;
+  }else if (result=='Tie'){
+    rpsscore.Tie+=1;
   }
+  
+  alert(`Your move is ${playermove} and computer move is ${computerMove}, so you ${result}.
+    Wins:${rpsscore.Wins} , Lose : ${rpsscore.Lose} , Tie :${rpsscore.Tie}`);
 }
 
 function pickcomputermove(){
@@ -45,11 +63,33 @@ function pickcomputermove(){
 }
 
 function headOrTail(){
+  let result = ''
   const random=Math.random();
   if (random>=0 && random <0.5){
-    alert(`You Win`);
+    result = 'You Win';
   }else if(random>=0.5 && random <1){
-    alert(`You Loose`);
+    result = 'You Lose';
   }
-  return eandom;
+  if (result=='You Win'){
+    htscore.Wins+=1;
+  }else if (result=='You Lose'){
+    htscore.Lose+=1;
+  }
+  alert(`${result}
+   Wins : ${htscore.Wins} , Lose : ${htscore.Lose} `)
+}
+function resetrpsScore() {
+  rpsscore.Wins = 0;
+  rpsscore.Lose = 0;
+  rpsscore.Tie = 0;
+  
+
+  alert("Scores Reset!\nRock Paper Scissors: Wins: 0, Losses: 0, Ties: 0");
+}
+function resethtScore() {
+  
+  htscore.Wins = 0;
+  htscore.Lose = 0;
+
+  alert("Scores Reset!\nHead or Tail: Wins: 0, Losses: 0");
 }
